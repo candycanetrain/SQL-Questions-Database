fad00ca54ed6764c393edb93e94aa3c729109932
Candra Tran <candratran@gmail.com>
2017-02-07 18:00:53 -0800

    working on bonus 1 (save)

--- model_base.rb
+++ model_base.rb
@@ -24,4 +24,31 @@ class ModelBase
     data.map {|item| self.new(item)}
   end
 
+  def save
+    instance_variables = self.instance_variables - [:@id]
+    values = instance_variables.map{ |name| instance_variable_get(name)}
+    names = instance_variables.map { |name| name.to_s[1..-1]}.join(', ')
+    q_marks = (['?']*instance_variables.length).join(', ')
+    if @id.nil?
+      #save it
+      QuestionsDatabase.instance.execute(<<-SQL, *values)
+        INSERT INTO
+          #{get_database_name} (#{names})
+        VALUES
+          (#{q_marks})
+      SQL
+      @id = QuestionsDatabase.instance.last_insert_row_id
+    else
+      #update it
+      QuestionsDatabase.instance.execute(<<-SQL, @fname, @lname, @id)
+        UPDATE
+          users
+        SET
+          fname = ?, lname = ?
+        WHERE
+          id = ?
+      SQL
+    end
+  end
+
 end
